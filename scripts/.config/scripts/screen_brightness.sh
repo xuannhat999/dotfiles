41edd30fce4740823fb590e0bf5a8bf2ae98e93c
xuannhat999 <xuannhat123456789@gmail.com>
2026-06-21 11:40:16 +0700

    .

--- scripts/.config/scripts/screen_brightness.sh
+++ scripts/.config/scripts/screen_brightness.sh
@@ -3,16 +3,9 @@
 brightness=$(brightnessctl get)
 max=$(brightnessctl max)
 percent=$((brightness * 100 / max))
-if [ $percent -le 20 ]; then
-  icon="/usr/share/icons/Papirus/48x48/status/notification-display-brightness-low.svg"
-elif [ $percent -le 60 ]; then
-  icon="/usr/share/icons/Papirus/48x48/status/notification-display-brightness-medium.svg"
-elif [ $percent -le 80 ]; then
-  icon="/usr/share/icons/Papirus/48x48/status/notification-display-brightness-high.svg"
-elif [ $percent -le 100 ]; then
-  icon="/usr/share/icons/Papirus/48x48/status/notification-display-brightness-full.svg"
-fi
+icon="/usr/share/icons/Papirus/48x48/status/notification-display-brightness-high.svg"
 notify-send \
   -i "$icon" \
   -h string:x-canonical-private-synchronous:brightness \
-  "Brightness" "$percent%" # -h int:value:$percent \
+  -h int:value:"$percent" \
+  "Brightness" "$percent%"
